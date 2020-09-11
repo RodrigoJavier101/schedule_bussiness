2fc0c23bcadea42b76caedc5c1faff0ddc6cdb02
Rodrigo <rodrigojaviergd@gmail.com>
2020-09-10 22:33:51 -0400

    agrego roomaplication al manifest

---
+++ app/src/main/java/com/rodrigo/javier/eox/hackermen/final_test_rodrigo_javier/model/room/RoomAplication.kt
@@ -0,0 +1,15 @@
+package com.rodrigo.javier.eox.hackermen.final_test_rodrigo_javier.model.room
+
+import android.app.Application
+import androidx.room.Room
+
+class RoomAplication : Application() {
+    companion object {
+        var gestionDatabase: GestionDatabase? = null
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        gestionDatabase = Room.databaseBuilder(this, GestionDatabase::class.java, "gestion-negocios-db").build()
+    }
+}
