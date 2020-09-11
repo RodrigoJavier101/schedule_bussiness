fecd40b9256a0d89e7ae273c3c44a14ea338508d
Rodrigo <rodrigojaviergd@gmail.com>
2020-09-11 17:31:14 -0400

    elimino ficheros room del ejemplo

--- app/src/main/java/com/rodrigo/javier/eox/hackermen/final_test_rodrigo_javier/PRUEBA_ROOM_EJERCICIO/RecommendationDataView.kt
+++
@@ -1,7 +0,0 @@
-package com.rodrigo.javier.eox.hackermen.final_test_rodrigo_javier.PRUEBA_ROOM_EJERCICIO
-
-data class RecommendationDataView(
-    var id: Long,
-    var recommendation: String,
-    var isCheck:Boolean
-)
