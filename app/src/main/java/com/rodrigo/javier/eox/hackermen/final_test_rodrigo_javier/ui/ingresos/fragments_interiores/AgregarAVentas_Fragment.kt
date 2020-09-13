bec4876af929c0a50d4249d6221debd60f97cb42
Rodrigo <rodrigojaviergd@gmail.com>
2020-09-12 23:08:06 -0400

    acomodo el layout de ingresos -lists y agregar

--- app/src/main/java/com/rodrigo/javier/eox/hackermen/final_test_rodrigo_javier/ui/ingresos/fragments_interiores/AgregarAVentas_Fragment.kt
+++ app/src/main/java/com/rodrigo/javier/eox/hackermen/final_test_rodrigo_javier/ui/ingresos/fragments_interiores/AgregarAVentas_Fragment.kt
@@ -6,25 +6,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AlertDialog
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.internal.runner.junit4.statement.UiThreadStatement
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.rodrigo.javier.eox.hackermen.final_test_rodrigo_javier.R
-import com.rodrigo.javier.eox.hackermen.final_test_rodrigo_javier.gestion_data_view.Agenda_DataView
 import com.rodrigo.javier.eox.hackermen.final_test_rodrigo_javier.model.adapters.AgendaRecyclerAdapter
 import com.rodrigo.javier.eox.hackermen.final_test_rodrigo_javier.model.room.GestionDao
 import com.rodrigo.javier.eox.hackermen.final_test_rodrigo_javier.model.room.GestionDatabase
 import com.rodrigo.javier.eox.hackermen.final_test_rodrigo_javier.model.room.RoomApplication
-import com.rodrigo.javier.eox.hackermen.final_test_rodrigo_javier.model.room.entities.Agenda_Entity
-import com.rodrigo.javier.eox.hackermen.final_test_rodrigo_javier.ui.agenda.AgendaViewModel
-import kotlinx.android.synthetic.main.add_asundo_agenda_layout_fake.view.*
-import kotlinx.android.synthetic.main.agregar_a_ventas_fragment.view.*
+
 
 class AgregarAVentas_Fragment : Fragment() {
 
-    private lateinit var agendaViewModel: AgendaViewModel
     private lateinit var recycler: RecyclerView
     private lateinit var adapter: AgendaRecyclerAdapter
     private lateinit var dataBase: GestionDatabase
@@ -55,91 +50,95 @@ class AgregarAVentas_Fragment : Fragment() {
     }
 
 
-    private fun setUpAddButton() {
-        /*   floatingActionButton.setOnClickListener {
-               val dialogView = layoutInflater
-                   .inflate(R.layout.add_asundo_agenda_layout_fake, null)
-               val agendaAsuntoInput = dialogView.agenda_asunto_input
-               val dialogBuilder = AlertDialog
-                   .Builder(requireContext())
-                   .setTitle("Agrega una Asunto a la Agenda")
-                   .setView(dialogView)
-                   .setNegativeButton("Cerrar") { dialog: DialogInterface, _: Int -> dialog.dismiss() }
-                   .setPositiveButton("Agregar") { dialog: DialogInterface, _: Int ->
+//    private fun setUpAddButton() {
+//        floatingActionButton.setOnClickListener {
+//            val dialogView = layoutInflater
+//                .inflate(R.layout.add_asundo_agenda_layout_fake, null)
+//            val agendaAsuntoInput = dialogView.agenda_asunto_input
+//            val dialogBuilder = AlertDialog
+//                .Builder(requireContext())
+//                .setTitle("Agrega una Asunto a la Agenda")
+//                .setView(dialogView)
+//                .setNegativeButton("Cerrar") { dialog: DialogInterface, _: Int -> dialog.dismiss() }
+//                .setPositiveButton("Agregar") { dialog: DialogInterface, _: Int ->
+//
+//                    if (agendaAsuntoInput.text?.isNotEmpty()!!) {
+//                        AsyncTask.execute {
+//                            val newItems = createEntityListFromDatabase(dao.getAllFromAgendaTable())
+//                            //                            var recomm =
+//                            //                                view?.findViewById<TextInputEditText>(R.id.agendamiento_box)
+//
+//                            if (agendaAsuntoInput != null) {
+//                                dao.insertAgenda(createEntity(agendaAsuntoInput.text.toString()))
+//                            }
+//
+//                            var thread = Thread() {
+//                                fun run() {
+//                                    try {
+//                                        synchronized(this) {
+//                                            Thread.sleep(500)
+//                                            UiThreadStatement.runOnUiThread {
+//                                                adapter.updateData(newItems)
+//                                                dialog.dismiss()
+//                                            }
+//                                        }
+//                                    } catch (e: InterruptedException) {
+//
+//                                    }
+//                                }
+//                            }
+//                            thread.start()
+//                        }
+//                    }
+//
+//                }
+//            dialogBuilder.create().show()
+//        }
+//
+//    }
 
-                       if (agendaAsuntoInput.text?.isNotEmpty()!!) {
-                           AsyncTask.execute {
-                               val newItems = createEntityListFromDatabase(dao.getAllFromAgendaTable())
-   //                            var recomm =
-   //                                view?.findViewById<TextInputEditText>(R.id.agendamiento_box)
+//    private fun createEntity(a_a: String): Agenda_Entity {
+//        return Agenda_Entity(asunto_agenda = a_a)
+//    }
 
-                               if (agendaAsuntoInput != null) {
-                                   dao.insertAgenda(createEntity(agendaAsuntoInput.text.toString()))
-                               }
+//    private fun createEntityListFromDatabase(registros_db: List<Agenda_Entity>):
+//            MutableList<Agenda_DataView> {
+//        val dataList = mutableListOf<Agenda_DataView>()
+//        if (registros_db.isNotEmpty()) {
+//            for (entity in registros_db) {
+//                val dataView = Agenda_DataView(
+//                    entity.id_agenda,
+//                    entity.fecha_programada.toString(),
+//                    entity.asunto_agenda
+//                )
+//                dataList.add(dataView)
+//            }
+//        }
+//        return dataList
+//
+//        val fab: FloatingActionButton = view.findViewById<FloatingActionButton>(R.id.fab_agregar)
+//        fab.setOnClickListener { view ->
+//            Toast.makeText(this, R.string.action_fab, Toast.LENGTH_SHORT).show()
+//        }
+//    }
 
-                               var thread = Thread() {
-                                   fun run() {
-                                       try {
-                                           synchronized(this) {
-                                               Thread.sleep(500)
-                                               UiThreadStatement.runOnUiThread {
-                                                   adapter.updateData(newItems)
-                                                   dialog.dismiss()
-                                               }
-                                           }
-                                       } catch (e: InterruptedException) {
+//    private fun createEntity(a_a: String): Agenda_Entity {
+//        return Agenda_Entity(asunto_agenda = a_a)
+//    }
 
-                                       }
-                                   }
-                               }
-                               thread.start()
-                           }
-                       }
-
-                   }
-               dialogBuilder.create().show()
-           }
-
-       }
-
-       private fun createEntity(a_a: String): Agenda_Entity {
-           return Agenda_Entity(asunto_agenda = a_a)
-       }
-
-       private fun createEntityListFromDatabase(registros_db: List<Agenda_Entity>):
-               MutableList<Agenda_DataView> {
-           val dataList = mutableListOf<Agenda_DataView>()
-           if (registros_db.isNotEmpty()) {
-               for (entity in registros_db) {
-                   val dataView = Agenda_DataView(
-                       entity.id_agenda,
-                       entity.fecha_programada.toString(),
-                       entity.asunto_agenda
-                   )
-                   dataList.add(dataView)
-               }
-           }
-           return dataList*/
-    }
-
-    private fun createEntity(a_a: String): Agenda_Entity {
-        return Agenda_Entity(asunto_agenda = a_a)
-    }
-
-    private fun createEntityListFromDatabase(registros_db: List<Agenda_Entity>):
-            MutableList<Agenda_DataView> {
-        val dataList = mutableListOf<Agenda_DataView>()
-        if (registros_db.isNotEmpty()) {
-            for (entity in registros_db) {
-                val dataView = Agenda_DataView(
-                    entity.id_agenda,
-                    entity.fecha_programada.toString(),
-                    entity.asunto_agenda
-                )
-                dataList.add(dataView)
-            }
-        }
-        return dataList
-    }
-
+//    private fun createEntityListFromDatabase(registros_db: List<Agenda_Entity>):
+//            MutableList<Agenda_DataView> {
+//        val dataList = mutableListOf<Agenda_DataView>()
+//        if (registros_db.isNotEmpty()) {
+//            for (entity in registros_db) {
+//                val dataView = Agenda_DataView(
+//                    entity.id_agenda,
+//                    entity.fecha_programada.toString(),
+//                    entity.asunto_agenda
+//                )
+//                dataList.add(dataView)
+//            }
+//        }
+//        return dataList
+//    }
 }
