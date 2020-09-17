f7e1c1d2af706828225c8b173a4ce629bdcbaba2
Rodrigo <rodrigojaviergd@gmail.com>
2020-09-17 18:32:52 -0400

    colores

--- app/src/main/java/com/rodrigo/javier/eox/hackermen/final_test_rodrigo_javier/ui/MainActivity.kt
+++ app/src/main/java/com/rodrigo/javier/eox/hackermen/final_test_rodrigo_javier/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.rodrigo.javier.eox.hackermen.final_test_rodrigo_javier.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.SystemClock.sleep
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
@@ -15,11 +16,15 @@ import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.rodrigo.javier.eox.hackermen.final_test_rodrigo_javier.R
+import kotlinx.coroutines.Delay
+import kotlinx.coroutines.InternalCoroutinesApi
+import java.lang.Thread.sleep
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
 
+    @InternalCoroutinesApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -55,7 +60,6 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this, "LoginOut", Toast.LENGTH_SHORT).show()
         var intent = Intent(this, LoginActivity::class.java)
         startActivity((intent))
+        finish()
     }
-
-
 }
