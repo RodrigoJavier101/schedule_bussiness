c6e4c258dca0323a27dd9199c77fe432ab37257c
Rodrigo <rodrigojaviergd@gmail.com>
2020-09-26 00:34:11 -0400

    borro una interface

--- app/src/main/java/com/rodrigo/javier/eox/hackermen/final_test_rodrigo_javier/Dos_/Interfaces.kt
+++
@@ -1,9 +0,0 @@
-package com.rodrigo.javier.eox.hackermen.final_test_rodrigo_javier.Dos_
-
-import com.rodrigo.javier.eox.hackermen.final_test_rodrigo_javier.Dos_.Clientes_Entity
-
-
-interface ItemClienteClickListener {
-    /*para actualizar user*/
-    fun itemClienteUpdateClick(cliente: Clientes_Entity?)
-}
